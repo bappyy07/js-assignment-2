da49fd8d5a1b97ffe87f3728afc0014c03ab6790
arnysamar@gmail.com <arnysamar@gmail.com>
2025-07-17 21:08:42 +0400

    event listener added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 const input = document.getElementById('todo-input');
 const addBtn = document.getElementById('add-btn');
 const todoList = document.getElementById('todo-list');
+
+
+addBtn.addEventListener('click', () => {
+  const text = input.value.trim();
+  if (text !== "") {
+    addTodoItem(text);
+    input.value = "";
+  }
+});
